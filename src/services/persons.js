e7dce2282c82c3cfe8365dbde7d7f63c4bdf8b10
iain-code <iainvirgo1010@live.co.uk>
2025-04-30 10:59:44 +0300

    scripts

--- src/services/persons.js
+++ src/services/persons.js
@@ -1,24 +1,24 @@
 import axios from "axios"
 
-const baseUrl = "http://localhost:3001"
+const baseUrl = "/api/persons"
 
 const getAll = () => {
-    const request = axios.get(baseUrl)
+    const request = axios.get("/")
     return request.then(response => response.data)
 }
 
 const create = (newObject) => {
-    const request = axios.post(`${baseUrl}/api/persons`, newObject)
+    const request = axios.post(`${baseUrl}`, newObject)
     return request.then(response => response.data)
 }
 
 const update = (id, newObject) => {
-    const request = axios.put(`${baseUrl}/api/persons/${id}`, newObject)
+    const request = axios.put(`${baseUrl}/${id}`, newObject)
     return request.then(response => response.data)
 }
 
 const del = (id) => {
-    const request = axios.delete(`${baseUrl}/api/persons/${id}`)
+    const request = axios.delete(`${baseUrl}/${id}`)
     return request.then(response => response.data)
 }
 
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: [
+      'phonebookfront-3.onrender.com',
+      // other allowed hosts if necessary
+    ],
+  },
 })
+
