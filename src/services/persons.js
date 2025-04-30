1ff2da640b2af28e8de0a5dd782ec77b62af4b67
iain-code <iainvirgo1010@live.co.uk>
2025-04-30 19:33:17 +0300

    changes to routes

--- src/services/persons.js
+++ src/services/persons.js
@@ -3,7 +3,7 @@ import axios from "axios"
 const baseUrl = "/api/persons"
 
 const getAll = () => {
-    const request = axios.get(baseUrl)
+    const request = axios.get()
     return request.then(response => response.data)
 }
 
